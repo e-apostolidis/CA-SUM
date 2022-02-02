e660321040767dec2b11ac71b66e5232c5291058
George Balaouras <gbalaouras@yahoo.com>
2022-02-02 13:17:53 +0200

    Update inference script regarding the correlation coefficients

--- inference/evaluation_metrics.py
+++ inference/evaluation_metrics.py
@@ -2,6 +2,7 @@
 import numpy as np
 import csv
 from scipy.stats import spearmanr, kendalltau, rankdata
+from collections import Counter
 
 
 def evaluate_summary(predicted_summary, user_summary, eval_method):
@@ -37,9 +38,9 @@ def evaluate_summary(predicted_summary, user_summary, eval_method):
 
 
 def get_corr_coeff(pred_imp_scores, video, dataset):
-    """ Read users annotations (frame-level importance scores) for each video in the dataset*. Compare the multiple
-    user annotations for the test video with the predicted frame-level importance scores of our CA-SUM for the same
-    video, by computing the Spearman's rho and Kendall's tau correlation coefficients. It must be noted, that the
+    """ Read users annotations (frame-level importance scores) for the `video` of the `dataset`* in use. Compare the
+    multiple user annotations for the test video with the predicted frame-level importance scores of our CA-SUM for the
+    same video, by computing the Spearman's rho and Kendall's tau correlation coefficients. It must be noted, that the
     calculated values are the average correlation coefficients over the multiple annotators.
     * Applicable only for the TVSum dataset.
 
@@ -52,29 +53,26 @@ def get_corr_coeff(pred_imp_scores, video, dataset):
     # Read the user annotations from the file
     annot_path = f".../CA-SUM/data/{dataset}/annotations/ydata-anno.tsv"
     with open(annot_path) as annot_file:
-        annot = csv.reader(annot_file, delimiter="\t")
-        names, user_scores = [], {}
-        for row in annot:
-            str_user = row[0]
+        user = int(video.split("_")[-1])
 
+        annot = list(csv.reader(annot_file, delimiter="\t"))
+        annotation_length = list(Counter(np.array(annot)[:, 0]).values())[user-1]
+        init = (user - 1) * annotation_length
+        till = user * annotation_length
+
+        user_scores = []
+        for row in annot[init:till]:
             curr_user_score = row[2].split(",")
             curr_user_score = np.array([float(num) for num in curr_user_score])
             curr_user_score = curr_user_score / curr_user_score.max(initial=-1)  # Normalize scores between 0 and 1
             curr_user_score = curr_user_score[::15]
 
-            if str_user not in names:
-                names.append(str_user)
-                user = f"video_{len(names)}"
-                user_scores[user] = [curr_user_score]
-            else:
-                user_scores[user].append(curr_user_score)
+            user_scores.append(curr_user_score)
 
     pred_imp_scores = np.array(pred_imp_scores)
-    curr_user_scores = user_scores[video]
-
     rho_coeff, tau_coeff = [], []
-    for annot in range(len(curr_user_scores)):
-        true_user_score = curr_user_scores[annot]
+    for annot in range(len(user_scores)):
+        true_user_score = user_scores[annot]
         curr_rho_coeff, _ = spearmanr(pred_imp_scores, true_user_score)
         curr_tau_coeff, _ = kendalltau(rankdata(pred_imp_scores), rankdata(true_user_score))
         rho_coeff.append(curr_rho_coeff)
